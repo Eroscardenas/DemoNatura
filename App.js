38a50552f2a7272133408338b851000f6f80dbc7
andreshdz1 <andreshermag@hotmail.com>
2024-04-09 19:22:55 -0600

    by andres

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
       <NavigationContainer>
         <StatusBar style="auto" />
         <Stack.Navigator
-          initialRouteName="Login"
+          initialRouteName="Loginnnn"
           screenOptions={{
             headerStyle: {
               backgroundColor: "#0C3A0A", // Change this to the color you want
